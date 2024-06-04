d06a2092ebf52b93e7a386d1eb45d22096790a8f
Harshit Vijay <harshitvj07@gmail.com>
2024-06-04 05:34:05 +0530

    Basic infinite reading input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("EnigmaSH Shell prototype");
+    // println!("EnigmaSH Shell prototype");
+    let mut input = String::new();
+    loop{
+        print!("EnigmaSH> ");
+        io::stdout().flush().expect("failed to push STDOUT");
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        // println!("OUT==>{input}");
+        input.clear();
+    }
 }
